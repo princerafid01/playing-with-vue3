301cdb88d626f2a8c1d038e9e49d5e05a4f827d8
Mahmud Hasan Rafid <mahmudrafid02@gmail.com>
2023-08-18 20:10:04 +0600

    Feat: Adding Tag filtering feature

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -16,9 +16,9 @@ export default {
     data() {
         return {
             assignments: [
-                { name: 'Finish project', complete: false, id: 1 },
-                { name: 'Read Chapter 4', complete: false, id: 2 },
-                { name: 'Turn in Homework', complete: false, id: 3 },
+                { name: 'Finish project', complete: false, id: 1, tag: 'Math' },
+                { name: 'Read Chapter 4', complete: false, id: 2, tag: 'Science' },
+                { name: 'Turn in Homework', complete: false, id: 3, tag: 'Math' },
             ],
         }
     },
